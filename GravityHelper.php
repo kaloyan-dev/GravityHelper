9aa653f9520ea89a41f259aee0a9a586c893de85
Kaloyan Ivanov <kaloyan.github@gmail.com>
2016-02-13 10:57:14 +0200

    Added entries retrieving function

--- GravityHelper.php
+++ GravityHelper.php
@@ -1,22 +1,30 @@
 <?php
 
 namespace GravityHelper;
+use GFFormsModel;
 
 class GravityHelper {
 
-	private $form_id = '';
-	private $options = array();
+	private $form_id      = false;
+	private $filter_affix = '';
+	private $options      = array();
 
 	function __construct( $id = false ) {
 		if ( $id ) {
-			$this->form_id = "_{$id}";
+			$this->form_id      = $id;
+			$this->filter_affix = "_{$id}";
 		}
 	}
 
 	public function button_submits() {
-		add_filter( "gform_submit_button{$this->form_id}", array( $this, 'form_submit_button' ), 10, 2 );
+		add_filter( "gform_submit_button{$this->filter_affix}", array( $this, 'form_submit_button' ), 10, 2 );
 
 		return $this;
+	}
+
+	public function form_submit_button( $button, $form ) {
+		$attributes = apply_filters( 'gh_button_atts', '' );
+		return "<button {$attributes} id='gform_submit_button_{$form['id']}'>{$form['button']['text']}</button>";
 	}
 
 	public function button_atts( $attributes ) {
@@ -38,8 +46,18 @@ class GravityHelper {
 		return implode( '', $attributes_html );
 	}
 
-	public function form_submit_button( $button, $form ) {
-		$attributes = apply_filters( 'gh_button_atts', '' );
-		return "<button {$attributes} id='gform_submit_button_{$form['id']}'>{$form['button']['text']}</button>";
+	public function get_entries() {
+		if ( ! class_exists( 'GFFormsModel' ) ) {
+			_e( 'Gravity Forms not active.', 'gh' );
+			return;
+		}
+
+		if ( ! $this->form_id ) {
+			_e( 'No form ID specified.', 'gh' );
+			return;
+		}
+
+		return GFFormsModel::get_leads( $this->form_id );
 	}
+
 }
