ad0c22ecb9137634d72b41cac7382af5a0ef61c9
Kaloyan Ivanov <kaloyan.github@gmail.com>
2016-02-13 10:39:27 +0200

    Added submit to button function

--- GravityHelper.php
+++ GravityHelper.php
@@ -4,4 +4,35 @@ namespace GravityHelper;
 
 class GravityHelper {
 
+	private $options;
+
+	public function button_submits() {
+		add_filter( 'gform_submit_button', array( $this, 'form_submit_button' ), 10, 2 );
+
+		return $this;
+	}
+
+	public function button_atts( $attributes ) {
+		$this->options['button_atts'] = $attributes;
+		add_filter( 'gh_button_atts', array( $this, 'apply_button_atts' ) );
+
+		return $this;
+	}
+
+	public function apply_button_atts() {
+		$attributes = $this->options['button_atts'];
+
+		$attributes_html = '';
+
+		foreach ( $attributes as $attribute => $value ) {
+			$attributes_html[] = "{$attribute}='{$value}'";
+		}
+
+		return implode( '', $attributes_html );
+	}
+
+	public function form_submit_button( $button, $form ) {
+		$attributes = apply_filters( 'gh_button_atts', '' );
+		return "<button {$attributes} id='gform_submit_button_{$form['id']}'>{$form['button']['text']}</button>";
+	}
 }
