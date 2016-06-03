30d4e40f352895ccd20552633aec611fd2cec0ad
Kaloyan Ivanov <kaloyanxivanov@gmail.com>
2016-06-03 20:58:34 +0300

    Improve entries retrieval

--- class/Entries.php
+++ class/Entries.php
@@ -30,17 +30,48 @@ class Entries extends GravityHelper {
 			return $data;
 		}
 
-		$field_data = array();
+		$field_data     = array();
+		$complex_fields = array();
 
 		foreach ( $fields['fields'] as $field ) {
 			$field_data[$field->id] = $field->label;
+
+			if ( ! empty( $field['inputs'] ) ) {
+				$complex_field_inputs = array();
+
+				foreach ( $field['inputs'] as $field_input ) {
+					$complex_field_inputs[] = array(
+						'id'    => $field_input['id'],
+						'label' => $field_input['label'],
+					);
+				}
+
+				$complex_fields[$field->id] = $complex_field_inputs;
+			}
 		}
 
 		foreach ( $entries as $entry ) {
 			$entry_data = array();
 
 			foreach ( $field_data as $id => $label ) {
-				$entry_data[$label] = $entry[$id];
+
+				$entry_content = $entry[$id];
+				
+				if ( in_array( $id, array_keys( $complex_fields ) ) ) {
+					$entry_content = array();
+
+					foreach ( $complex_fields[$id] as $input_data ) {
+
+						$input_checked = empty( $entry[ $input_data['id'] ] ) ? 0 : 1;
+
+						$entry_content[] = array(
+							'label'   => $input_data['label'],
+							'checked' => $input_checked,
+						);
+					}
+				}
+
+				$entry_data[$label] = $entry_content;
 			}
 
 			$data[$entry['id']] = $entry_data;
